86474df88d9b734bc0005a253fa3afdbd11d8909
alexaleluia12 <alexaleluiaforgit@gmail.com>
2016-08-26 21:07:03 -0300

    atualizacao codigo pythong

--- quadrado.py
+++ quadrado.py
@@ -1,33 +1,35 @@
-#-*- coding:utf-8 -*-
-
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+
+# TODO
+# atualizar o angular js tmb *
 
 
 class Quadrado(object):
-    _size   = 9
-    _rangeNun = 1
-    _initDefault = 1
-    _endDefault = 10
-    def getList(self, init, end):
-        """ return a list of int between (init, end) inclusive 
-        
-        Diference between end and init should be 9, otherwise 
+    _end_default = 10
+    _size = 9
+    _range_nun = 1
+    _init_default = 1
+
+    def get_list(init, end):
+        """ return a list of int between (init, end) inclusive
+
+        Diference between end and init should be 9, otherwise
         [1,2...10] given.
         """
-        sanityInt = end - init
-        if self._size == sanityInt:
-            return list(range(init, end + self._rangeNun))
-        return list(range(self._initDefault, 
-                          self._endDefault + self._rangeNun))
+        sanity_int = end - init
+        if Quadrado._size == sanity_int:
+            return list(range(init, end + Quadrado._range_nun))
+        return list(range(Quadrado._init_default,
+                          Quadrado._end_default + Quadrado._range_nun))
 
 app = Flask(__name__)
+CORS(app)
 
-
-square = Quadrado()
 
 @app.route("/")
 def home():
-    return render_template('index.html', numList=square.getList(1, 10))
+    return render_template('index.html', numList=Quadrado.get_list(1, 10))
 
 
 @app.route('/numdata')
@@ -35,9 +37,9 @@ def api():
     lst = []
     init = request.args.get('init', 1, type=int)
     end = request.args.get('end', 10, type=int)
-    lst = square.getList(init, end)
+    lst = Quadrado.get_list(init, end)
     return jsonify(numList=lst)
-    
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
